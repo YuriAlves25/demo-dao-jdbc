8fa2fdd1c42653d760fd5c8c39841f4bc9775966
YuriAlves25 <yuri_alves00@hotmail.com>
2024-01-25 17:29:28 -0300

    findById impementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,11 +10,13 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
 
-        Seller seller = new Seller(21, "bob", "bob@hotmail.com", new Date(), 3000.0, obj);
+
+
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.finById(3);
 
         System.out.println(seller);
     }
--- src/main/java/org/example/model/dao/DaoFactory.java
+++ src/main/java/org/example/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package org.example.model.dao;
 
+import org.example.db.DB;
 import org.example.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
